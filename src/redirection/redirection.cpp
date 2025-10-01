4d881f57651097fc7ca80caae4996fe49d9c2d80
Xuanyu Quah <xquah@u90z03s01.42kl.edu.my>
2025-10-01 16:32:16 +0800

    chore: added swithc cases to redirect and minor change to main.html

--- src/redirection/redirection.cpp
+++ src/redirection/redirection.cpp
@@ -3,7 +3,26 @@
 void redirection(Response &response, Request &request, Location location) {
     response.setVersion(request.getVersion());
     response.setStatusCode(location.getRedirectCode());
-    response.setStatusMessage("Moved Permanently");
+    // response.setStatusMessage("Moved Permanently");
+    switch (location.getRedirectCode()) {
+        case 301:
+            response.setStatusMessage("Moved Permanently");
+            break;
+        case 302:
+            response.setStatusMessage("Found");
+            break;
+        case 303:
+            response.setStatusMessage("See Other");
+            break;
+        case 307:
+            response.setStatusMessage("Temporary Redirect");
+            break;
+        case 308:
+            response.setStatusMessage("Permanent Redirect");
+            break;
+        default:
+            response.setStatusMessage("Redirect");
+    }
     response.addHeader("Connection", "keep-alive");
     response.addHeader("Content-Length", to_string(response.getBody().length()));
     response.addHeader("Content-Type", "text/html"); //**Might not be 100% correct */
